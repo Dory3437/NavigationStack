a37529b329cb8fcd12b16a1cd8dfe5c1e82a7f19
Sofia <ephermiral@gmail.com>
2023-07-27 13:45:57 -0400

    Initial Commit

---
+++ NavigationStack/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  NavigationStack
+//
+//  Created by scholar on 7/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationStack {
+            VStack {
+                Text("This is the root view🌳")
+                
+                
+                
+                
+            }.toolbar {
+                
+                ToolbarItemGroup(placement: .status) {
+                    
+                    NavigationLink(destination: SecondView()) {
+                        Text("Click me!")
+                    }
+                    
+                    NavigationLink(destination: Text("This is the page I added myself!")) {
+                        Text("Press me?") }
+                    
+                }
+                
+            }//closing toolbar
+            
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(false)
+            
+        }//closing Navstack
+    }//closing some view
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationStack/SecondView.swift
@@ -0,0 +1,21 @@
+//
+//  SecondView.swift
+//  NavigationStack
+//
+//  Created by scholar on 7/27/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Welcome to the second view!")
+    }
+    
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
